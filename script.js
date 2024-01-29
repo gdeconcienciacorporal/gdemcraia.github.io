2f5ce26067e4c657f716ad909b9b6d814d98ceb7
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-01-29 11:27:21 -0300

    Semana 4 y 5

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4','Dia 5', 'Dia 6'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en column-container-1',
-    data: [11, 17, 4.7, 16, 10, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 4',
+    data: [17, 22, 6.4, 24, 12, 12.3], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: false,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4','Dia 5', 'Dia 6'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en column-container-2',
-    data: [13, 19, 17, 9.4, 14, 20], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 5',
+    data: [20, 15, 19, 13, 22, 12.3], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: false,
     tension: 0.1
@@ -50,13 +50,6 @@ const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
     }
   }
 });
-function onOpen() {
-  var ui = SpreadsheetApp.getUi();
-  ui.createMenu('Menu Personalizado')
-       .addItem('Agregar Marca de Agua', 'addWatermark')
-       .addToUi();
- }
- 
 
 function actualizarDiasRestantes() {
   // Obtener el título que contiene la fecha objetivo
