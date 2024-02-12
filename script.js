5b011257025f1d4f1bff7d2d217e28c842028ece
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-02-12 12:30:55 -0300

    Semana 6 y 7

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4','Dia 5', 'Dia 6'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 4',
-    data: [17, 22, 6.4, 24, 12, 12.3], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 6',
+    data: [7, 9, 10, 0, 60, 0], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: false,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4','Dia 5', 'Dia 6'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 5',
-    data: [20, 15, 19, 13, 22, 12.3], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 7',
+    data: [11, 17, 17, 17, 13, 24], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: false,
     tension: 0.1
