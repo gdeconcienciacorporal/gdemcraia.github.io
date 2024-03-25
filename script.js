422f5f40004a61eb5078d2d4594017c33081a7ed
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-03-24 23:02:37 -0300

    Semana 12 y 13

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4','Dia 5', 'Dia 6'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 10',
-    data: [12, 15, 15, 20, 13, 26], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 12',
+    data: [15, 13, 10, 18, 10, 20], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: false,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4','Dia 5', 'Dia 6'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 11',
-    data: [16, 22, 17, 14, 10, 25 ], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 13',
+    data: [7, 12, 6, 0, 3, 73.8], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: false,
     tension: 0.1
