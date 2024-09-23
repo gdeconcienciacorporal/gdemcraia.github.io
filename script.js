4b42a6134c5031bc62307ab19e061b35ebecc7fc
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-09-23 10:45:39 -0300

    Commit weeks 34 & 35

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ actualizarDiasRestantes();
 setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
 //Datos para el gráfico de líneas
 const dataLine1 = {
-labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
+labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6'], // Etiquetas de los días
 datasets: [{
-label: 'Km recorridos en Semana 32',
-data: [10, 13, 11, 11, 14], // Datos de los km recorridos en cada día
+label: 'Km recorridos en Semana 34',
+data: [16, 21, 19, 18, 23, 25], // Datos de los km recorridos en cada día
 borderColor: 'rgba(75, 192, 192, 1)',
 fill: true,
 tension: 0.1
@@ -14,10 +14,10 @@ tension: 0.1
 };
 
 const dataLine2 = {
-labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
+labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6'], // Etiquetas de los días
 datasets: [{
 label: 'Km recorridos en Semana 33',
-data: [14, 20, 12, 20, 19], // Datos de los km recorridos en cada día
+data: [10, 20, 18, 26, 19, 18], // Datos de los km recorridos en cada día
 borderColor: 'rgba(153, 102, 255, 1)',
 fill: true,
 tension: 0.1
