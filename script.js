04e746a807dae4641fc81b17c7d0c367170fb2e0
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-12-16 10:09:36 -0300

    Commit weeks 46 & 47

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-actualizarDiasRestantes();
-setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
+//actualizarDiasRestantes();
+//setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
 //Datos para el gráfico de líneas
 const dataLine1 = {
-labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
+labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6'], // Etiquetas de los días
 datasets: [{
-label: 'Km recorridos en Semana 44',
-data: [6, 7, 6, 4, 30], // Datos de los km recorridos en cada día
+label: 'Km recorridos en Semana 46',
+data: [12, 18, 10, 15, 10, 13], // Datos de los km recorridos en cada día
 borderColor: 'rgba(75, 192, 192, 1)',
 fill: true,
 tension: 0.1
@@ -16,8 +16,8 @@ tension: 0.1
 const dataLine2 = {
 labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6'], // Etiquetas de los días
 datasets: [{
-label: 'Km recorridos en Semana 45',
-data: [9, 15, 11, 12, 9, 18], // Datos de los km recorridos en cada día
+label: 'Km recorridos en Semana 47',
+data: [11, 15, 15, 18, 11, 15], // Datos de los km recorridos en cada día
 borderColor: 'rgba(153, 102, 255, 1)',
 fill: true,
 tension: 0.1
